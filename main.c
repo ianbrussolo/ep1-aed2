3ce629d623ad5b2b6b62a28edc616dbed8b64369
Ian Tiozzo <ianbrussolo@hotmail.com>
2023-06-01 16:32:54 -0300

    modified:   .vscode/launch.json 	modified:   Makefile 	modified:   grafo_listaadj.c 	deleted:    grafo_listaadj.exe 	modified:   grafo_listaadj.h 	deleted:    make.bat 	deleted:    treste.c

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "grafo_listaadj.h"
+// #include "grafo_matrizadj.h"
+
+/* void trataResultadosBusca(Apontador aresta) {
+  for(int i = 0; i <= aresta->size; i++) {
+    if(aresta-> >= 450) aresta->listaAdj[i].weight = 450;
+    else if(aresta->listaAdj[i].weight >= 400) aresta->listaAdj[i].weight = 400;
+    else if(aresta->listaAdj[i].weight >= 350) aresta->listaAdj[i].weight = 350;
+    else if(aresta->listaAdj[i].weight >= 300) aresta->listaAdj[i].weight = 300;
+    else if(aresta->listaAdj[i].weight >= 250) aresta->listaAdj[i].weight = 250;
+    else aresta->listaAdj[i].weight = -1;
+  }
+} */
+
+int main(int argc, char const *argv[])
+{   
+    FILE* fp = fopen(argv[1], "r");
+    
+    /* le primeira linha do arquivo de entrada */
+    int numVertices, numArestas, numConsultas;
+    fscanf(fp, "%d %d %d", &numVertices, &numArestas, &numConsultas);
+
+    /* inicializa grafo */
+    Grafo *grafo = (Grafo *)malloc(sizeof(Grafo));
+    inicializaGrafo(&grafo, numVertices);
+
+    /* le arestas */
+    int v1, v2;
+    float peso;
+    for (int i = 0; i < numArestas; i++) {
+        printf("dentro do for loop\n");
+        fscanf(fp, "%d %d %f", &v1, &v2, &peso);
+        insereAresta(v1, v2, peso, &grafo);
+    }
+    printf("leu e armazenou no grafo\n");
+
+    for (int i = 0; i < numConsultas; i++) {
+        fscanf(fp, "%d %d\n", &v1, &v2);
+        // Process the query data here
+    }
+    /* depois de ler todo o arquivo não esqueça de fechá-lo */
+    fclose(fp);
+
+
+
+    return 0;
+}
